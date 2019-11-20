f2e616900d6bd8eecc75642c3e14c132d4c30b60
Luis Valero <luisvaleroc22@gmail.com>
2019-11-20 14:55:28 -0300

    tercer commit

--- App.js
+++ App.js
@@ -3,6 +3,23 @@ import { AppLoading } from 'expo';
 import { Container, Text } from 'native-base';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
+import { createStackNavigator, createSwitchNavigator, createDrawerNavigator, NavigationActions } from 'react-navigation';
+
+const AppSwitchNavigator = createSwitchNavigator({
+  Home
+}, { initialRouteName: 'Loading' });
+
+const AppSwitchNavigator = createSwitchNavigator({
+  Home
+}, { initialRouteName: 'Loading' });
+const DrawerStack = createDrawerNavigator({
+  //menudrawell
+}, {
+  headerMode: 'float',
+  drawerPosition: 'left',
+  contentComponent: props => <SideBar {...props} />,
+});
+
 
 export default class App extends React.Component {
   constructor(props) {
@@ -12,6 +29,22 @@ export default class App extends React.Component {
     };
   }
 
+
+  componentWillMount = async () => {
+    const AccesToken = await getTokenData();
+    initInterceptor(() => {
+      this.navigatorRef.dispatch(NavigationActions.navigate({ routeName: 'Login' }));
+    });
+
+    if (AccesToken) {
+      setAuthorizationHeader(AccesToken.access_token);
+    }
+  }
+  componentDidMount() {
+    this.notificationSubscription = Notifications.addListener(this.handleNotification);
+  }
+
+
   async componentDidMount() {
     await Font.loadAsync({
       Roboto: require('native-base/Fonts/Roboto.ttf'),
@@ -19,6 +52,9 @@ export default class App extends React.Component {
       ...Ionicons.font,
     });
     this.setState({ isReady: true });
+
+    his.notificationSubscription = Notifications.addListener(this.handleNotification);
+
   }
 
   render() {
